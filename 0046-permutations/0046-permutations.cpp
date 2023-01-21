12ae367916efa6886d029c3d4aa62aa4c5e5f283
NIKHILmgs <74446466+NIKHILmgs@users.noreply.github.com>
2023-01-21 21:53:17 +0530

    Time: 3 ms (79.01%), Space: 7.6 MB (92.45%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
-    void permut(vector<vector<int>>&ans, int arr[],vector<int>v, vector<int>nums)
+//     void permut(vector<vector<int>>&ans, int arr[],vector<int>v, vector<int>nums)
+//     {
+//         if(v.size()==nums.size())
+//         {
+//             // cout<<"I am here"<<"\n";
+//             ans.push_back(v);
+//             return;
+//         }
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(arr[i]!=1)
+//             {
+//                 arr[i]=1;
+//                 v.push_back(nums[i]);
+//                 permut(ans,arr,v,nums);
+//                 arr[i]=0;
+//                 v.pop_back();
+//             }
+//         }
+//     }
+    void permut(int idx, vector<vector<int>>&ans,vector<int>&nums)
     {
-        if(v.size()==nums.size())
+        if(idx==nums.size())
         {
-            // cout<<"I am here"<<"\n";
-            ans.push_back(v);
+            ans.push_back(nums);
             return;
         }
-        
-        for(int i=0;i<nums.size();i++)
+        for(int i=idx;i<nums.size();i++)
         {
-            if(arr[i]!=1)
-            {
-                arr[i]=1;
-                v.push_back(nums[i]);
-                permut(ans,arr,v,nums);
-                arr[i]=0;
-                v.pop_back();
-            }
+            swap(nums[idx],nums[i]);
+            permut(idx+1,ans,nums);
+            swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
@@ -31,7 +45,8 @@ public:
         vector<int>v;
         vector<vector<int>>ans;
         
-        permut(ans,freq_array,v,nums);
+        // permut(ans,freq_array,v,nums);
+        permut(0,ans,nums);
         
         return ans;
         
