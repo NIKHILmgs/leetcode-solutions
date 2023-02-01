a0c754a3c514d6a149265b4150a6a185ddfccb38
NIKHILmgs <74446466+NIKHILmgs@users.noreply.github.com>
2023-02-01 22:10:15 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.04%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n  = __gcd(str1.size(),str2.size());
+        string s = str1.substr(0,n);
+        if((str1+str2)==str2+str1)
+            return s;
+        return "";
+        
+    }
+};
