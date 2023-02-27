50a731f3ad4872b0dfcd1f4d599e95236441a281
NIKHILmgs <74446466+NIKHILmgs@users.noreply.github.com>
2023-02-27 22:55:34 +0530

    Time: 8 ms (74.42%), Space: 13.7 MB (24.49%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int fo(vector<int>&nums,int t){
+        int n=nums.size();
+        int l=0;int h=n-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==t){
+                if(m==0){
+                    return m;
+                }
+                else{
+                    if(nums[m-1]!=t)
+                        return m;
+                    else
+                        h=m-1;
+                }
+            }
+            else if(nums[m]>t)
+                h=m-1;
+            else
+                l=m+1;
+        }
+        return -1;
+    }
+    int lo(vector<int>&nums,int t){
+        int n=nums.size();
+        int l=0;int h=n-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==t){
+                if(m==n-1){
+                    return m;
+                }
+                else{
+                    if(nums[m+1]!=t)
+                        return m;
+                    else
+                        l=m+1;
+                }
+            }
+            else if(nums[m]>t)
+                h=m-1;
+            else
+                l=m+1;
+        }
+        return -1;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int a=fo(nums,target);
+        ans.push_back(a);
+        a=lo(nums,target);
+        ans.push_back(a);
+        return ans;
+        
+    }
+};
