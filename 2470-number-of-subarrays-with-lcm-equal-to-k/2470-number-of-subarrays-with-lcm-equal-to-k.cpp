dad7275cd8ae4a8edb20c5f6fbd71a69526d93e7
Nikhil Kumar Mishra <74446466+NIKHILmgs@users.noreply.github.com>
2023-03-11 21:38:14 +0530

    Time: 30 ms (62.01%), Space: 9.7 MB (30.64%) - LeetHub

---
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int subarrayLCM(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        int ans=0;
+        for (int i = 0; i < n; i++) {
+            int curr_lcm = nums[i];
+             if(curr_lcm==k)
+                    ans++;
+            for (int j = i+1; j < n; j++) {
+                curr_lcm = lcm(curr_lcm,nums[j]);
+                if(curr_lcm==k)
+                    ans++;
+                else if(curr_lcm>k)
+                    break;
+            }
+        }
+        return ans;
+        
+        
+    }
+};
