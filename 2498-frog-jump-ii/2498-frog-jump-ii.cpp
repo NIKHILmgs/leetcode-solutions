b12b833ba06b0107d8de4a7be2724c8cf8614777
Nikhil Kumar Mishra <74446466+NIKHILmgs@users.noreply.github.com>
2023-03-13 09:31:06 +0530

    Time: 116 ms (99.39%), Space: 65.3 MB (68.75%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -4,11 +4,10 @@ public:
   
     int maxJump(vector<int>& stones) {
         int n = stones.size();
-        if(n==2)
-            return stones[1]-stones[0];
-        int mx = INT_MIN;
-        for(int i=2;i<n;i++)
-            mx = max(mx,(stones[i]-stones[i-2]));
+       int mx = stones[1]-stones[0];
+       
+        for(int i=1;i<n-1;i++)
+            mx = max(mx,(stones[i+1]-stones[i-1]));
         return mx;
         
     }
