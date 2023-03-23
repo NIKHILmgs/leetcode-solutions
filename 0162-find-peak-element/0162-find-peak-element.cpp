ca8d059ba0ca756357a3118e48717c222c6d62cd
Nikhil Kumar Mishra <74446466+NIKHILmgs@users.noreply.github.com>
2023-03-23 12:45:19 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (78.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+//         int l = 0,h=nums.size()-1,n = h+1;
+//         while(l<h)
+//         {
+//             int mid = l+(h-l)/2;
+//             if(mid == 0)
+//             {
+//                return (nums[mid]>=nums[mid+1])?0:1;
+//             }
+//             if(mid == n-1)
+//             {
+//                 return (nums[mid]>=nums[mid-1])?n-1:n-2;
+//             }
+//             if(nums[mid]>=nums[mid-1] and nums[mid]>=nums[mid+1])
+//                 return mid;
+//             if(nums[mid]<nums[mid-1])
+//                 h = mid-1;
+//             else
+//                 l = mid+1;
+            
+//         }
+//          return l;
         int l = 0,h=nums.size()-1,n = h+1;
         while(l<h)
         {
             int mid = l+(h-l)/2;
-            if(mid == 0)
-            {
-               return (nums[mid]>=nums[mid+1])?0:1;
-            }
-            if(mid == n-1)
-            {
-                return (nums[mid]>=nums[mid-1])?n-1:n-2;
-            }
-            if(nums[mid]>=nums[mid-1] and nums[mid]>=nums[mid+1])
+           
+            if((mid == 0 or (nums[mid]>nums[mid-1])) and ((mid == nums.size()-1 ) or (nums[mid]>nums[mid+1])))
                 return mid;
-            if(nums[mid]<nums[mid-1])
-                h = mid-1;
-            else
+            if(nums[mid+1]>nums[mid])
                 l = mid+1;
+            else
+                h = mid-1;
             
         }
          return l;
+        
     }
 };
